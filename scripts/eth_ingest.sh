d321a325ff5173be77ec85986ffada3f45aa998b
mdragaschnig <m.dragaschnig@posteo.net>
2021-05-06 14:23:00 +0200

    update eth_ingest.sh for dockerised usage

--- scripts/eth_ingest.sh
+++ scripts/eth_ingest.sh
@@ -1,31 +1,8 @@
-#!/bin/bash
+#!/bin/sh
+echo "Ingesting from ${PROVIDER_URL} into Cassandra keyspace ${RAW_KEYSPACE} on ${CASSANDRA_HOST}"
 
-# settings
-ETH_ETL=/usr/local/bin/ethereumetl
-DS_BULK=/usr/local/bin/dsbulk
-CASSANDRA_HOST=
-CASSANDRA_KEYSPACE=eth_raw
-START_BLOCK=0
-END_BLOCK=
-PROVIDER_URI=file:///var/data/geth/geth.ipc
-
-# ingest
-
-## blocks
-$ETH_ETL export_blocks_and_transactions \
-  --start-block $START_BLOCK \
-  --end-block $END_BLOCK \
-  --blocks-output - \
-  --provider-uri $PROVIDER_URI | \
-  $DS_BULK load -c csv -header true \
-    -h $CASSANDRA_HOST -k $CASSANDRA_KEYSPACE -t block
-
-## transactions
-$ETH_ETL export_blocks_and_transactions \
-  --start-block $START_BLOCK \
-  --end-block $END_BLOCK \
-  --transactions-output - \
-  --provider-uri $PROVIDER_URI | \
-  $DS_BULK load -c csv -header true \
-    -h $CASSANDRA_HOST -k $CASSANDRA_KEYSPACE -t transaction \
-    --connector.csv.maxCharsPerColumn=-1
+python3 -u /usr/local/bin/eth_ingest.py \
+    -d $CASSANDRA_HOST \
+    -k $RAW_KEYSPACE \
+    -p $PROVIDER_URL \
+    -u yesterday
