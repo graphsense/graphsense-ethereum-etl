867ec0020d75a1b3e6bf80dc69fbd7e6316e51c3
Rainer Stuetz <rainer.stuetz@ait.ac.at>
2021-05-07 13:49:43 +0200

    Rename script

--- scripts/eth_ingest.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-echo "Ingesting from ${PROVIDER_URL} into Cassandra keyspace ${RAW_KEYSPACE} on ${CASSANDRA_HOST}"
-
-python3 -u /usr/local/bin/eth_ingest.py \
-    -d $CASSANDRA_HOST \
-    -k $RAW_KEYSPACE \
-    -p $PROVIDER_URL \
-    -u yesterday
