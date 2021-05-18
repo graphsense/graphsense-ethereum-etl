7599c6272827b081869f504d1bb2230a5065c580
Rainer Stuetz <rainer.stuetz@ait.ac.at>
2021-05-18 09:01:46 +0200

    Edit Docker ingest script

--- scripts/ingest.sh
+++ scripts/ingest.sh
@@ -2,8 +2,8 @@
 echo "Ingesting from ${PROVIDER_URI} into Cassandra keyspace ${RAW_KEYSPACE} on ${CASSANDRA_HOST}"
 
 python3 -u /usr/local/bin/eth_ingest.py \
-    -l "$HOME/dsbulk" \
+    -l "$HOME/logs" \
     -d "$CASSANDRA_HOST" \
     -k "$RAW_KEYSPACE" \
-    -p "$PROVIDER_URI" \
+    -p file:///opt/geth/geth.ipc \
     -u yesterday
