fe9d9e808e302240821a16b7cc1fdf5f2cba01a4
Rainer Stuetz <rainer.stuetz@ait.ac.at>
2021-11-08 11:48:17 +0100

    Improve CSV import script

--- scripts/csv_import.sh
+++ scripts/csv_import.sh
@@ -8,30 +8,20 @@ fi
 DIR=$1
 DB_HOST=$2
 DB_KEYSPACE=$3
-DSBULK="dsbulk"
 
-find "$DIR" -name 'block_*.csv.gz' -type f -print | sort |
-    while read -r filename; do
-        echo "### $filename"
-        zcat "$filename" |
-            "$DSBULK" load -c csv -header true \
-                -h "$DB_HOST" -k "$DB_KEYSPACE" -t block
-    done
+dsbulk load -c csv -h "$DB_HOST" -k "$DB_KEYSPACE" -t block -url "$DIR" \
+    --connector.csv.fileNamePattern '**/block_*.csv.gz' \
+    --connector.csv.compression gzip \
+    --connector.csv.recursive true
 
-find "$DIR" -name 'tx_*.csv.gz' -type f -print | sort |
-    while read -r filename; do
-        echo "### $filename"
-        zcat "$filename" |
-            "$DSBULK" load -c csv -header true \
-                -h "$DB_HOST" -k "$DB_KEYSPACE" -t transaction \
-                --connector.csv.maxCharsPerColumn=-1
-    done
+dsbulk load -c csv -h "$DB_HOST" -k "$DB_KEYSPACE" -t transaction -url "$DIR" \
+    --connector.csv.fileNamePattern '**/tx_*.csv.gz' \
+    --connector.csv.compression gzip \
+    --connector.csv.recursive true \
+    --connector.csv.maxCharsPerColumn=-1
 
-find "$DIR" -name 'trace_*.csv.gz' -type f -print | sort |
-    while read -r filename; do
-        echo "### $filename"
-        zcat "$filename" |
-            "$DSBULK" load -c csv -header true \
-                -h "$DB_HOST" -k "$DB_KEYSPACE" -t trace \
-                --connector.csv.maxCharsPerColumn=-1
-    done
+dsbulk load -c csv -h "$DB_HOST" -k "$DB_KEYSPACE" -t trace -url "$DIR" \
+    --connector.csv.fileNamePattern '**/trace_*.csv.gz' \
+    --connector.csv.compression gzip \
+    --connector.csv.recursive true \
+    --connector.csv.maxCharsPerColumn=-1
